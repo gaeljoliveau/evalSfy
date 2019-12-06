46276acc766aae49a18da0cc93d4df257c97bbbe
gael <gael.joliveau@icloud.com>
2019-12-06 18:21:44 +0100

    Finalisation de la partie œuvres + ajout du controller exposition

--- src/Controller/ArtworkController.php
+++ src/Controller/ArtworkController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArtworkRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,24 +13,44 @@ class ArtworkController extends AbstractController
     /**
      * @Route("/artwork", name="artwork.index")
      */
-    public function index(ArtworkRepository $repository ):Response
+    public function index(ArtworkRepository $repository, CategoryRepository $categoryRepository):Response
         {
+            $categories = $categoryRepository->findAll();
+
             $result = $repository->findAll();
             return $this->render('artwork/index.html.twig',[
-                'artworks' => $result
+                'artworks' => $result,
+                'categories' => $categories
             ]);
     }
 
     /**
-     * @Route("/artwork/{id}", name="artwork/detail")
+     * @Route("/artwork/category/{catId}", name="artwork.indexfiltered")
+     */
+    public function indexfiltered(ArtworkRepository $repository, CategoryRepository $categoryRepository, int $catId ):Response{
+
+        $categories = $categoryRepository->findAll();
+        $cat = $categoryRepository->find($catId);
+
+        $result = $repository->findBy([
+            'category' => $cat
+        ]);
+
+        return $this->render('artwork/index.html.twig',[
+            'artworks' => $result,
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @Route("/artworkdetail/{id}", name="artwork.detail")
      */
     public function detail(ArtworkRepository $artworkRepository,int $id):Response
     {
-        $result = $artworkRepository->findOneBy([
-           'id' => $id
-        ]);
-        return $this->render('artwork/index.html.twig',[
-            'artworks' => $result
+        $result = $artworkRepository->find($id);
+
+        return $this->render('artwork/artwork.html.twig',[
+            'artwork' => $result
         ]);
     }
 }
