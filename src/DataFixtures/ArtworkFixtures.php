bab4dc2ea024c1437a6ded6562a229c3678951cb
gael <gael.joliveau@icloud.com>
2019-12-06 11:25:04 +0100

    Fixtures sur œuvres + réel noms de catégories

---
+++ src/DataFixtures/ArtworkFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Artwork;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Faker\Factory as Faker;
+
+class ArtworkFixtures extends Fixture implements OrderedFixtureInterface
+{
+
+    public $categories = ["paintCategory", "drawingCategory", "sculptureCategory"];
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $artwork = new Artwork();
+
+            $artwork
+                ->setName($faker->unique()->sentence(3))
+                ->setDescription($faker->text)
+                ->setPicture($faker->image('public/img/artwork/', 800, 450, null, false));
+
+            $randomCategoryIndex = random_int(0, count($this->categories) - 1);
+            $artwork->setCategory($this->getReference($this->categories[$randomCategoryIndex]));
+
+            $manager->persist($artwork);
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+
+}
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -4,22 +4,49 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory as Faker;
 
-class CategoryFixtures extends Fixture
+class CategoryFixtures extends Fixture implements OrderedFixtureInterface
 {
+
     public function load(ObjectManager $manager)
     {
-        for($i = 0; $i < 5; $i++){
-            $category = new Category();
-            $category->setName("catégorie$i");
 
-            // créer une référence pour mettre en relation les entités : mise en mémoire de l'instance
-            $this->addReference("category$i", $category);
 
-            $manager->persist($category);
-        }
+        $category = new Category();
+        $category->setName("Peinture");
+
+        // créer une référence pour mettre en relation les entités : mise en mémoire de l'instance
+        $this->addReference("paintCategory", $category);
+
+        $manager->persist($category);
+
+
+        $category2 = new Category();
+        $category2->setName("Dessin");
+
+        // créer une référence pour mettre en relation les entités : mise en mémoire de l'instance
+        $this->addReference("drawingCategory", $category2);
+
+        $manager->persist($category2);
+
+
+        $category3 = new Category();
+        $category3->setName("Sculpture");
+
+        // créer une référence pour mettre en relation les entités : mise en mémoire de l'instance
+        $this->addReference("sculptureCategory", $category3);
+
+        $manager->persist($category3);
+
 
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
     }
 }
